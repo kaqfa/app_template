144b6f5ef5ec9df71550903d280e36b39d69b413
fahri firdausillah <elfaatta@gmail.com>
2017-06-16 09:03:10 +0700

    edit text

--- AplikasiKeuangan/app/src/main/java/id/co/keuangan/aplikasikeuangan/LoginActivity.java
+++ AplikasiKeuangan/app/src/main/java/id/co/keuangan/aplikasikeuangan/LoginActivity.java
@@ -4,13 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtEmail, edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtEmail = (EditText) findViewById(R.id.log_email);
+        edtPassword = (EditText) findViewById(R.id.log_password);
     }
 
     public void toRegister(View view){
--- AplikasiKeuangan/app/src/main/java/id/co/keuangan/aplikasikeuangan/RegisterActivity.java
+++ AplikasiKeuangan/app/src/main/java/id/co/keuangan/aplikasikeuangan/RegisterActivity.java
@@ -3,14 +3,21 @@ package id.co.keuangan.aplikasikeuangan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText txtEmail, txtPassword, txtConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtEmail = (EditText) findViewById(R.id.reg_email);
+        txtPassword = (EditText) findViewById(R.id.reg_password);
+        txtConfirm = (EditText) findViewById(R.id.reg_confirm);
     }
 
     public void toLogin(View view){
